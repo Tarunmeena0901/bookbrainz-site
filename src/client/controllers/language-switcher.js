d41dc2295e7ec83455d08debb2215094c575d0bc
Tarunmeena0901 <meenatarun0901@gmail.com>
2024-03-15 15:06:46 +0530

    change translation file folder path inside public/locales

---
+++ src/client/controllers/language-switcher.js
@@ -0,0 +1,23 @@
+import { useTranslation } from "react-i18next";
+
+
+const lngs = {
+    en: { nativeName: 'English' },
+    es: { nativeName: 'Spanish' }
+};
+
+
+function languageSwitcher() {
+    const { i18n } = useTranslation();
+    return (
+        <div>
+            {Object.keys(lngs).map((lng) => (
+                <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
+                    {lngs[lng].nativeName}
+                </button>
+            ))}
+        </div>
+    )
+}
+
+export default languageSwitcher;
